5409d877b76752ad2ce16a07a6464c380182165a
Sava Balasevic <lorsosolutions@gmail.com>
2017-04-28 00:37:55 +0200

    pocelo

--- novo/Form1.Designer.cs
+++ novo/Form1.Designer.cs
@@ -52,7 +52,7 @@
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(32, 131);
+            this.checkBox2.Location = new System.Drawing.Point(146, 129);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(80, 17);
             this.checkBox2.TabIndex = 2;
